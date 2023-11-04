5d308dda7867b4e8e7df03f4a725060f031425bc
anton amir <amiranton257@gmail.com>
2023-11-05 01:49:09 +0200

    resolve vercel Error 3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,15 +9,15 @@ interface Inputs {
   password: string;
 }
 
+const [Login, setLogin] = useState(false);
+const { signIn, signUp } = useAuth();
+const {
+  register,
+  handleSubmit,
+  watch,
+  formState: { errors },
+} = useForm<Inputs>();
 function login() {
-  const [Login, setLogin] = useState(false);
-  const { signIn, signUp } = useAuth();
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
     if (Login) {
       await signIn(email, password);
