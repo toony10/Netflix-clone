bce4f3b7ecae999d0ef6d7e8e78283999b2e1c69
anton amir <amiranton257@gmail.com>
2023-11-04 17:21:52 +0200

    built a custom hook (useAuth)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,8 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { useForm, SubmitHandler } from "react-hook-form";
-
+import { ExclamationCircleIcon } from "@heroicons/react/20/solid";
+import useAuth from "@/hooks/userAuth";
 interface Inputs {
   email: string;
   password: string;
@@ -10,14 +11,20 @@ interface Inputs {
 
 function login() {
   const [login, setLogin] = useState(false);
-
+  const { signIn, signUp } = useAuth();
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    if (login) {
+      // await singIn(email,password)
+    } else {
+      // await singUp(email,password)
+    }
+  };
   return (
     <div className="relative flex w-screen h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Image
@@ -36,23 +43,60 @@ function login() {
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
       />
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
         <h1 className="text-4xl font-semibold">Sing in</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", {
+                required: { value: true, message: "this filled is required" },
+              })}
+            />
+            {errors.email && (
+              <div className="flex">
+                <ExclamationCircleIcon className="h-5 w-5 my-auto space-x-2 text-orange-500" />
+                <p className="p-1 text-[13px] font-light  text-orange-500">
+                  {errors.email.message}
+                </p>
+              </div>
+            )}
           </label>
 
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", {
+                required: { value: true, message: "this filled is required" },
+              })}
+            />
+            {errors.password && (
+              <div className="flex">
+                <ExclamationCircleIcon className="h-5 w-5 my-auto space-x-2 text-orange-500" />
+                <p className="p-1 text-[13px] font-light  text-orange-500">
+                  {errors.password.message}
+                </p>
+              </div>
+            )}
           </label>
         </div>
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}>
           Sing In
         </button>
         <div className="text-[gray]">
           New To Netflix?{" "}
-          <button type="submit" className="text-white  hover:underline">
+          <button
+            type="submit"
+            className="text-white  hover:underline"
+            onClick={() => setLogin(false)}>
             Sing up now
           </button>
         </div>
---
+++ hooks/userAuth.tsx
@@ -0,0 +1,117 @@
+"use client";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from "firebase/auth";
+import { useRouter } from "next/router";
+import { auth } from "../firebase";
+
+interface IAuth {
+  user: User | null;
+  signUp: (email: string, password: string) => Promise<void>;
+  signIn: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+  error: string | null;
+  loading: boolean;
+}
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+const AuthContext = createContext<IAuth>({
+  user: null,
+  signUp: async () => {},
+  signIn: async () => {},
+  logOut: async () => {},
+  error: null,
+  loading: false,
+});
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  // Persisting the user
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // Logged in...
+          setUser(user);
+          setLoading(false);
+        } else {
+          // Not logged in...
+          setUser(null);
+          setLoading(true);
+          router.push("/login");
+        }
+
+        setInitialLoading(false);
+      }),
+    [auth]
+  );
+  const signUp = async (email: string, password: string) => {
+    setLoading(true);
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  };
+
+  const signIn = async (email: string, password: string) => {
+    setLoading(true);
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setUser(userCredential.user);
+        router.push("/");
+        setLoading(false);
+      })
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  };
+  const logOut = async () => {
+    setLoading(true);
+    signOut(auth)
+      .then(() => setUser(null))
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  };
+
+  const memoedValue = useMemo(
+    () => ({
+      user,
+      signUp,
+      signIn,
+      loading,
+      logOut,
+      error,
+    }),
+    [user, loading]
+  );
+  return (
+    <AuthContext.Provider value={memoedValue}>
+      {!initialLoading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export default function useAuth() {
+  return useContext(AuthContext);
+}
